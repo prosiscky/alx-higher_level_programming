e4592ea35bc59260f6a9e452b9a8612ecb123e27
prosiscky <legendaryprosperity001@gmail.com>
2023-01-02 18:37:08 -0800

    A code that prints a substring from a string

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:65] + ' ' +str[-22:-19] + ' ' + str[:5]
+str = str[39:65] + ' ' + str[-22:-19] + ' ' + str[:5]
 print(str)
