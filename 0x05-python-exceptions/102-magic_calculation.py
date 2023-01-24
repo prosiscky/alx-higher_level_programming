6766df74d5541d388baaddf33f208a0c1028e72b
prosiscky <legendaryprosperity001@gmail.com>
2023-01-23 18:19:56 -0800

    A function that replicates the steps of a given python bytecode.

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    res = 0
+    result = 0
     for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
             else:
-                res += a ** b / i
-            except Exception:
-                res = b + a
-                break
-    return (res)
+                result += a ** b / i
+        except Exception:
+            result = b + a
+            break
+    return (result)
