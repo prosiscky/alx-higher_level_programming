fffd3c3acbbd2a289480cd080bc417f841bd981d
prosiscky <legendaryprosperity001@gmail.com>
2023-04-12 15:17:46 -0700

    A script that defines an empty class "Rectangle"

---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// An empty class 'Rectangle' that defines a rectangle
+
+// Requirement
+// - You must use the class notation for defining your class
+
+class Rectangle {
+}
+
+module.exports = Rectangle;
