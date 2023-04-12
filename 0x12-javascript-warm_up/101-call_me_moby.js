ad0583c9fd0ce7f1d2dffa0faaa62634c574521b
prosiscky <legendaryprosperity001@gmail.com>
2023-04-11 21:57:57 -0700

    A function that increments and calls a function.

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // A function that executes x times a function
 // The function must be visible from outside with prototype:
-//function (x, theFunction)
+// function (x, theFunction)
 
 exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < x; i++) {
