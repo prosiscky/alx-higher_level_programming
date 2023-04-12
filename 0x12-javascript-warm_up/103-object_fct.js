a4e18f47434562129f6024381b3077f806e90398
prosiscky <legendaryprosperity001@gmail.com>
2023-04-11 21:55:17 -0700

    adding a new function incr that increments the integer value.

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// Update the given script by adding a new function incr that increments the integer value.
+
+// You are not allowed to use var
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
