695fd8d4dbdc1868507f1e436421ee460603773d
prosiscky <legendaryprosperity001@gmail.com>
2023-04-11 21:19:29 -0700

    A  script that computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// A script that computes and prints a factorial
+
+// The first argument is integer (argument can be cast as integer) used for computing the factorial
+// Factorial of NaN is 1
+// You must do it recursively
+// You must use a function
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+
+function factorial (n) {
+  if (n < 0) {
+    return (-1);
+  }
+  if (n === 0 || isNaN(n)) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
+}
+
+console.log(factorial(Number(process.argv[2])));
