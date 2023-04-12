171862a6d60cfe3ce38135f40203ca30bbbd9dd5
prosiscky <legendaryprosperity001@gmail.com>
2023-04-11 19:06:05 -0700

    A script that prints two arguments passed to it in the following format: arg1 “ is ” arg2

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// A script that prints two arguments passed to it, in the following format: arg1 “ is ” arg2
+// - You must use console.log(...) to print all output
+// - You are not allowed to use var
+
+console.log(process.argv[2] + ' is ' + process.argv[3]);
