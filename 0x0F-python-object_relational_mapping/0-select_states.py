9cf90c2e46482826462ddb48d46e63729d9f6878
prosiscky <legendaryprosperity001@gmail.com>
2023-04-16 20:51:41 -0700

    A script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,15 +6,15 @@ import sys
 The states represents the columns in the Table
 """
 # REQUIREMENTS
-    '''
-    - This script should take 3 arguments:
-    mysql username, mysql password and database name
-    (no argument validation needed)
+'''
+- This script should take 3 arguments:
+mysql username, mysql password and database name
+(no argument validation needed)
 
-    - The result of the query is ordered by the Auto generated
-    id hence no need for order by clause
-    '''
-if __name__=="__main__":
+- The result of the query is ordered by the Auto generated
+id hence no need for order by clause
+'''
+if __name__ == "__main__":
     dbase = MySQLdb.connect(
         user=sys.argv[1],
         passwd=sys.argv[2],
@@ -22,10 +22,10 @@ if __name__=="__main__":
         host="localhost",
         port=3306
     )
-    dbObj = dbase.cursor()
-    dbObj.execute("SELECT * FROM states")
-    stateList = dbObj.fetchall()
-    for state in stateList:
+    obj = dbase.cursor()
+    obj.execute("SELECT * FROM states")
+    records = obj.fetchall()
+    for state in records:
         print(state)
-    dbObj.close()
+    obj.close()
     dbase.close()
