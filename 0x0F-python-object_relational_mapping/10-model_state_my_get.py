5af32705384ffb8b39bee59a011ea2f8613e2a7b
prosiscky <legendaryprosperity001@gmail.com>
2023-04-17 16:46:46 -0700

    prints the State object passed as argument from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" A  script that prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa
+REQUIREMENTS
+- Your script should take 4 arguments: mysql username,
+mysql password, database name and state name to search (SQL injection free)
+- You must use the module SQLAlchemy
+- You must import State and Base from model_state - from
+model_state import Base, State
+- Your script should connect to a MySQL server running on localhost at
+port 3306
+- You can assume you have one record with the state name to search
+- Results must display the states.id
+- If no state has the name you searched for, display Not found
+- Your code should not be executed when imported
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    instance = session.query(State).filter(State.name == (sys.argv[4],))
+    try:
+        print(instance[0].id)
+    except IndexError:
+        print("Not found")
