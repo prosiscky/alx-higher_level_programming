d42c4ac295719405a03e1d0215a982716259f0f1
prosiscky <legendaryprosperity001@gmail.com>
2023-04-17 07:55:47 -0700

    A script that searches but safe from Injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""A script that  takes in an argument and
+displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+- The script should be safe from MySQL injections!
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    dbase = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        host='localhost',
+        port=3306
+    )
+    state = sys.argv[4]
+    dbObj = dbase.cursor()
+    dbObj.execute("SELECT * FROM states WHERE Binary name LIKE %s", (state, ))
+    stateList = dbObj.fetchall()
+    for records in stateList:
+        print(records)
+
+    dbObj.close()
+    dbase.close()
