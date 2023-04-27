25c0ab5a2a64562b1d1cf594194d109487a4d5e8
prosiscky <legendaryprosperity001@gmail.com>
2023-04-27 19:36:50 +0000

    script that takes in a URL and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,6 +2,5 @@
 # A Bash script that takes in a URL, sends a request to that URL
 #and displays the size of the body of the response
 
-size=$(curl -s "$1" | tr -d '\r' | wc -c)
-echo "$size"
+curl -s "$1" | tr -d '\r' | wc -c
 
