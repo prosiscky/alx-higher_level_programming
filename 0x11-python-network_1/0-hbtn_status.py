ebfc17198d867e10b46fc00aa3fd120e703d57d0
prosiscky <legendaryprosperity001@gmail.com>
2023-04-28 23:56:12 +0000

    A script that fetches a url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,7 @@
-#!/usr/bin/p ython3
+#!/usr/bin/python3
 
 
-"""Pytihon script that fetches https://alx-intranet.hbtn.io/status
+"""Python script that fetches https://alx-intranet.hbtn.io/status
 REQUIREMENTS:
 You must use the package urllib
 You are not allowed to import any packages other than urllib
@@ -11,13 +11,15 @@ You must use a with statement"""
 
 import urllib.request
 
+
 url = "https://alx-intranet.hbtn.io/status"
 
-with urllib.request.urlopen(url) as resp:
-    content = resp.read()
-        utf8_content = content.decode('utf-8')
+with urllib.request.urlopen(url) as response:
+    content = response.read()
+    utf8_content = content.decode('utf-8')
 
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+print("Body response:")
+print("\t- type: {}".format(type(content)))
+print("\t- content: {}".format(content))
+print("\t- utf8 content: {}".format(utf8_content.decode('utf-8')))
+
