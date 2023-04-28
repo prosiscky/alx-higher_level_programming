cfeee461396489ac1ca582451448d9ab74d5e627
prosiscky <legendaryprosperity001@gmail.com>
2023-04-28 01:52:41 +0000

    A script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!
-curl -s -X PUT 0.0.0.0:5000/catch_me -H "Origin:HolbertonSchool" -d "user_id=98" -L -H "Referer: 0.0.0.0:5000/catch_me" -w "\n" -o /dev/null
+curl -s -H "Origin: HolbertonSchool" -H "Referer: 0.0.0.0:5000/catch_me" -d "user_id=98" 0.0.0.0:5000/catch_me -o /dev/null
