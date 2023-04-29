5a1a216e8f33ac64ac64ba7092e8a9c8321258ef
prosiscky <legendaryprosperity001@gmail.com>
2023-04-29 00:10:34 +0000

    A script that fetches a url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -12,14 +12,14 @@ You must use a with statement"""
 import urllib.request
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
 
-    with urllib.request.urlopen(url) as resp:
-        content = resp.read()
-        utf8_content = content.decode("utf-8")
+	url = "https://alx-intranet.hbtn.io/status"
 
-    print('Body response:')
-    print('\t- type: {}'.format(type(content)))
-    print('\t- content: {}'.format(content))
-    print('\t- utf8 content: {}'.format(utf8_content.decode("utf-8")))
+	with urllib.request.urlopen(url) as resp:
+    		content = respo.read()
+    		utf8_content = content.decode('utf-8')
 
+	print("Body response:")
+	print("\t- type: {}".format(type(content)))
+	print("\t- content: {}".format(content))
+	print("\t- utf8 content: {}".format(utf8_content)))
