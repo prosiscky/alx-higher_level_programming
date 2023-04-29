d0b6e3822fe506c3958784ba424c326f9746ac12
prosiscky <legendaryprosperity001@gmail.com>
2023-04-29 01:03:00 +0000

    A script that sends a request to a URL and displays the body of the response (decoded in utf-8).

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,7 +3,8 @@
 
 """
  A Python script that takes in a URL,
-sends a request to the URL and displays the body of the response (decoded in utf-8).
+sends a request to the URL and displays
+the body of the response (decoded in utf-8).
 REQUIREMENTS:
 You have to manage urllib.error.HTTPError exceptions and print:
 Error code: followed by the HTTP status code
