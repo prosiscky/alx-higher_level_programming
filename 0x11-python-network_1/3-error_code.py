bd3266a56172b0ede5a33775f0e02e1007171ff6
prosiscky <legendaryprosperity001@gmail.com>
2023-04-29 01:01:05 +0000

    A script that sends a request to a URL and displays the body of the response (decoded in utf-8).

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+"""
+ A Python script that takes in a URL,
+sends a request to the URL and displays the body of the response (decoded in utf-8).
+REQUIREMENTS:
+You have to manage urllib.error.HTTPError exceptions and print:
+Error code: followed by the HTTP status code
+You must use the packages urllib and sys
+You are not allowed to import other packages than urllib and sys
+You don’t need to check arguments passed to the script (number or type)
+You must use the with statement
+"""
+
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
+
