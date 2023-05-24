38e7df85d2fb09c354e043fffb68c1b75fe25d7c
Prosiscky <legendaryprosperity001@gmail.com>
2023-05-24 03:28:54 +0000

    A script that writes a string to a file.

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,15 @@
+#!/iusr/bin/node
+// a script that writes data to a file
+
+// Import object fs into a variable fs
+const fs = require('fs');
+
+// create a variable file to hold file path
+const file = process.argv[2];
+
+// write to file
+fs.writeFile(file, process.argv[3], { flag: 'w' }, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
